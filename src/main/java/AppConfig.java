08df004a5b57303f2c3f0df8e56a7f155eeb97f2
Khaizar1 <khaizar@mail.ru>
2022-07-19 21:38:53 +0300

    Дубль №1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,6 +2,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.io.UTFDataFormatException;
+
 @Configuration
 public class AppConfig {
  
@@ -11,4 +13,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
+    }
+
 }
